a299e94491a592c271ac181c44af81d1426c7a4b
Misachka <136197530+Misachka@users.noreply.github.com>
2023-09-26 18:36:44 -0400

    started creating routes, updated apiroutes.js

---
+++ routes/apiroutes.js
@@ -0,0 +1,40 @@
+
+// dependencies 
+const path = require('path');
+const fs = require('fs')
+
+// creates unique ids
+var uniqid = require('uniqid');
+
+
+// routing
+module.exports = (app) => {
+
+  // GET /api/notes will read the db.json file and return all saved notes as JSON.
+  app.get('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../db/db.json'));
+  });
+
+  // POST /api/notes should receive a new note to save on the request body and adds it to db.json file, and it will be returned as a new note to client.
+  app.post('/api/notes', (req, res) => {
+    let db = fs.readFileSync('db/db.json');
+    db = JSON.parse(db);
+    res.json(db);
+    // creating body for note
+    let userNote = {
+      title: req.body.title,
+      text: req.body.text,
+      // creating unique id for each note
+      id: uniqid(),
+    };
+    // pushing created note to  the db.json file
+    db.push(userNote);
+    fs.writeFileSync('db/db.json', JSON.stringify(db));
+    res.json(db);
+
+  });
+
+};
+
+
+
