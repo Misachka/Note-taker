504129fb1b4dc28c3d3cdd412f2724a3a104d0ad
Misachka <136197530+Misachka@users.noreply.github.com>
2023-10-21 14:05:09 -0400

    updated server.js and added clog middleware

--- server.js
+++ server.js
@@ -1,26 +1,37 @@
-// Dependencies
 const express = require('express');
+const path = require('path');
+const { clog } = require('./middleware/clog');
+const api = require('./routes/index.js');
 
-// app use express
-const app = express();
-
-// creating environment variable port
 const PORT = process.env.PORT || 3001;
 
+const app = express();
 
-// asks express to create a route for every file in the 'public' folder
-app.use(express.static('public'));
-// sets up express app to handle data parser
-app.use(express.urlencoded({ extended: true }));
+// Import custom middleware, "cLog"
+app.use(clog);
+
+// Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
 
+app.use(express.static('public'));
 
-// routes 
-require('./routes/apiroutes')(app);
-require('./routes/htmlroutes')(app);
+// GET Route for homepage
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
 
+// GET Route for feedback page
+app.get('/feedback', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/pages/feedback.html'))
+);
 
-//  starts the server
-app.listen(PORT, () => {
-  console.log(`Server available at localhost${PORT}`);
-});
+// Wildcard route to direct users to a 404 page
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/pages/404.html'))
+);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
